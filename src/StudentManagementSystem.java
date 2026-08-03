44f6e14242584a8c07cd28cb3b4fecb27d8dcb61
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-03 15:06:00 +0400

    working on 339

--- src/StudentManagementSystem.java
+++ src/StudentManagementSystem.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class StudentManagementSystem {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String [] studentNames;
+        Integer studentCount;
+        Integer choice;
+        String studentName;
+        Integer index;
         
     }
 }
