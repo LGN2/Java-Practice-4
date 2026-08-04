8bed408a532189d2d80be2c76275fb65352694d8
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-04 12:09:30 +0400

    Working on 340

--- src/ScoreManagementSystem.java
+++ src/ScoreManagementSystem.java
@@ -24,6 +24,78 @@ public class ScoreManagementSystem {
         studentScores.add(98);
         studentScores.add(70);
 
-        
+        do {
+
+            System.out.println("\n========== SCORE MANAGEMENT SYSTEM ==========");
+            System.out.println("1. Display Scores");
+            System.out.println("2. Analyze Scores");
+            System.out.println("3. Update Score");
+            System.out.println("4. Search Score");
+            System.out.println("5. Remove Score");
+            System.out.println("6. Sort Scores");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+
+                    displayScores(studentScores);
+                    break;
+
+                case 2:
+
+                    analyzeScores(studentScores);
+                    break;
+
+                case 3:
+
+                    System.out.print("Enter Index: ");
+                    index = sc.nextInt();
+
+                    System.out.print("Enter New Score: ");
+                    score = sc.nextInt();
+
+                    updateScore(studentScores, index, score);
+                    break;
+
+                case 4:
+
+                    System.out.print("Enter Score To Search: ");
+                    score = sc.nextInt();
+
+                    searchScore(studentScores, score);
+                    break;
+
+                case 5:
+
+                    System.out.print("Enter Index To Remove: ");
+                    index = sc.nextInt();
+
+                    removeScore(studentScores, index);
+                    break;
+
+                case 6:
+
+                    sortScores(studentScores);
+                    break;
+
+                case 7:
+
+                    System.out.println("Program Ended.");
+                    break;
+
+                default:
+
+                    System.out.println("Invalid Menu Choice.");
+            }
+
+        } while (choice != 7);
+
+        sc.close(); 
     }
+
+}
 }
