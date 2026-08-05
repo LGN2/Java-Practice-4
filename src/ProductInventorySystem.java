b46e5f7773b8a2f3122dede2e2319907d022a262
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-05 11:55:41 +0400

    Working on 341

--- src/ProductInventorySystem.java
+++ src/ProductInventorySystem.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 public class ProductInventorySystem {
     public static void main(String[] args){
 
-        ArrayList<String> productNames;
-        ArrayList<Integer> productQuantities;
-        ArrayList<Double> productPrices;
+        ArrayList<String> productNames = null;
+        ArrayList<Integer> productQuantities = null;
+        ArrayList<Double> productPrices = null;
 
-        
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
     }
 }
