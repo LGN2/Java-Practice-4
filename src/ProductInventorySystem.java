8b594caade37157ad1c15b7bdc9d053a2cc7e78b
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-05 12:02:43 +0400

    Working on 341

--- src/ProductInventorySystem.java
+++ src/ProductInventorySystem.java
@@ -7,9 +7,46 @@ public class ProductInventorySystem {
         ArrayList<String> productNames = null;
         ArrayList<Integer> productQuantities = null;
         ArrayList<Double> productPrices = null;
-
+        //p1
         productNames.add("Laptop");
         productQuantities.add(5);
         productPrices.add(750.50);
+        //p2
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p3
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p4
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p5
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p6
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p7
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p8
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p9
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+        //p10
+        productNames.add("Laptop");
+        productQuantities.add(5);
+        productPrices.add(750.50);
+       
     }
 }
